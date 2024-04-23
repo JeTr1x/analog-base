e08be2e0b199610dc6a3ef0bb0e04eb128d307f5
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-04-23 14:04:02 +0300

    Update Counter.sol

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,7 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IGmpReceiver} from "@analog-gmp/interfaces/IGmpReceiver.sol";
+
+interface IGmpReceiver {
+    /**
+     * @dev Handles the receipt of a single GMP message.
+     * The contract must verify the msg.sender, it must be the Gateway Contract address.
+     *
+     * @param id The EIP-712 hash of the message payload, used as GMP unique identifier
+     * @param network The chain_id of the source chain who send the message
+     * @param source The pubkey/address which sent the GMP message
+     * @param payload The message payload with no specified format
+     * @return 32 byte result which will be stored together with GMP message
+     */
+    function onGmpReceived(bytes32 id, uint128 network, bytes32 source, bytes calldata payload)
+        external
+        payable
+        returns (bytes32);
+}
 
 contract Counter is IGmpReceiver {
     address private immutable _gateway;
@@ -17,3 +33,4 @@ contract Counter is IGmpReceiver {
         return bytes32(number);
     }
 }
+
